426c72483dfb07e23cb1cff12ddabd9a3ccf19bf
colinecaillier <coline.caillier@gmail.com>
2016-12-06 11:56:53 +0100

    I'm workiiiiinnng !!!!!

--- Js/sketch.js
+++ Js/sketch.js
@@ -1,13 +1,12 @@
 function setup() {
 
-  var mymap = L.map('mapid').setView([46.2148, 6.1506], 11);
-
-  L.tileLayer('https://api.mapbox.com/styles/v1/colinecaillier/ciwcn7t0f00ax2qpm7n00tcvh/tiles/256/{level}/{col}/{row}@2x?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+  var mapid = L.map('mapid').setView([46.2148, 6.1506], 11);
+  L.tileLayer('https://api.mapbox.com/styles/v1/colinecaillier/ciwcn7t0f00ax2qpm7n00tcvh/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA',{
+    attribution:'fond',
+    maxZoom: 22,
     id: 'colinecaillier.ciwcossds06h62olkl109i7bz-60liw',
     accessToken: 'pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA'
-}).addTo(mymap);
+}).addTo(mapid);
 
 
 
