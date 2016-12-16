f5577cd54d09b123447123a37bd13af04858d060
colinecaillier <coline.caillier@gmail.com>
2016-12-16 22:48:37 +0100

    Creation Layer

--- Js/sketch.js
+++ Js/sketch.js
@@ -1,37 +1,47 @@
 
-var mapid = L.map('mapid');
 
 function setup(){
-  var urlFond ='https://api.mapbox.com/styles/v1/colinecaillier/ciwcn7t0f00ax2qpm7n00tcvh/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA';
-  var urlBench ='https://api.mapbox.com/styles/v1/colinecaillier/ciwlxt0ap00072qqyrdaqhl3s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA';
+  var urlFond ='https://api.mapbox.com/styles/v1/colinecaillier/ciwcn7t0f00ax2qpm7n00tcvh/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA',
+      fondAttrib = 'Mapbox fond',
+      urlBench='https://api.mapbox.com/styles/v1/colinecaillier/ciwlxt0ap00072qqyrdaqhl3s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA',
+      benchAttrib = 'Mapbox Bench';
 
+  var Fond = L.tileLayer(urlFond,{
+    attribution:fondAttrib,
+    id: 'colinecaillier.ciwcossds06h62olkl109i7bz-60liw',
+    accessToken: 'pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA'
+  });
+  var Bench = L.tileLayer(Bench,{
+    attribution:benchAttrib ,
+      id: 'colinecaillier.ciwcossds06h62olkl109i7bz-60liw',
+      accessToken: 'pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA'
+  });
 
-mapid.setView([46.2148, 6.1506], 11);
+var mapid = L.map('mapid',{
+  layers : [Fond]
+}).setView([46.2148, 6.1506],11);
 
-// calque fond
-L.tileLayer(urlFond,{
-  attribution:'fond',
-  maxZoom: 22,
-  id: 'colinecaillier.ciwcossds06h62olkl109i7bz-60liw',
-  accessToken: 'pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA'
+var baseLayer ={
+    "Background" : Fond,
+    "Bench" : Bench
+};
+  //var mapid = L.map('mapid').fitWorld();
+//  map.locate({setView: true, maxZoom: 16});
 
-}).addTo(mapid);
+/*
+var mapid = L.map('mapid', {
+    center: [46.2148, 6.1506],
+    maxZoom:22,
 
-// calque bench
-L.tileLayer(urlBench,{
-  attribution:'bench',
-  maxZoom: 22,
-  id: 'colinecaillier.ciwcossds06h62olkl109i7bz-60liw',
-  accessToken: 'pk.eyJ1IjoiY29saW5lY2FpbGxpZXIiLCJhIjoiY2l3Y242eGdwMDA0YjJ0bnhraDF1NDlxMyJ9.FQLJag67Cka7KRcY-ZiKsA'
-}).addTo(mapid);
+    layers: [Fond]
+});*/
 
-$('.pure-button').on('click', function(){
-  mapid.locate({setView: true, maxZoom: 15});
-});
-}
-mapid.on('locationfound', onLocationFound);
-function onLocationFound(e) {
-    console.log(e);
-    // e.heading will contain the user's heading (in degrees) if it's available, and if not it will be NaN. This would allow you to point a marker in the same direction the user is pointed.
-    L.marker(e.latlng).addTo(mapid);
+print('yo im working')
+
+var maps = {
+    "Fond": Fond,
+  //  "Bench": Bench
+};
+var controlLayers = L.control.layers(maps).addTo(mapid);
+
 }
